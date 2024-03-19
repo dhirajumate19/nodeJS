4cd8365b6677603e55f725329a4f2ff4ea0aeeb1
Dhiraj Umate <62473097+dhirajumate19@users.noreply.github.com>
2024-03-19 10:54:30 +0530

    Create day4CreateServer.js
    create first server in node js

---
+++ questions/nodejs/day4CreateServer.js
@@ -0,0 +1,16 @@
+import { createServer } from "node:http";
+const PORT = 8000;
+
+//create method for server and handler request and response object
+const handlerServer = (req, rep) => {
+  console.log("hello\n Welcome to our first server");
+  rep.end("hello\n Welcome to our first server");
+};
+
+//Creating server using http create method
+const server = createServer(handlerServer);
+
+//listerning server detail like port and other detail
+server.listen(PORT, () => {
+  console.log(`The server is running on localhost:${PORT}`);
+});
